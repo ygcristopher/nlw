ed89ce2e2c22f6ba772ed3c0a044dd171c7a4f6a
ygcristopher <yago1900@gmail.com>
2023-01-22 11:35:21 -0300

    add readme

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
-  water: ["02-04"],
-  food: ["01-05"],
-  ready: ["03-06"],
-  study: ["01-07"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  // const today = "22/01"
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists) {
+   alert("Dia já incluso")
+   return
+  }
+
+  alert('Adicionado com sucesso ✅')
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load() 
